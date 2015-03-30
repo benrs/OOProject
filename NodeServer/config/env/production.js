b5220e6f5856a6f3ddbb02afa7679a9e0d9ac0e3
Benjamin Barault <benjaminbarault@gmail.com>
2015-03-29 21:26:08 -0400

    Pushing code

--- NodeServer/config/env/production.js
+++ NodeServer/config/env/production.js
@@ -2,11 +2,9 @@ var port = 8080;
 var serverIP   = 'localhost';
 var dbIP       = '54.186.35.160';
 var dbPort     = '27017';
-var dbUser     = 'ubuntu';
-var dbPassword = 'barault6404'; 
 
 module.exports = {
 	port: port,
 	serverIP: serverIP,
-	db: "mongodb://"+dbUser+":"+dbPassword+"@"+dbIP+":"+dbPort+"/picApp"
+	db: "mongodb://"+dbIP+":"+dbPort+"/picApp"
 }
