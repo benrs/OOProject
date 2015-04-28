d975db6026d035a11fea4782edd15d6b1fd615c4
Benjamin Barault <benjaminbarault@gmail.com>
2015-04-27 23:16:44 -0400

    Pushing database fix

--- NodeServer/config/env/production.js
+++ NodeServer/config/env/production.js
@@ -1,6 +1,6 @@
 var port = 8080;
 var serverIP   = 'localhost';
-var dbIP       = 'ec2-54-200-0-50.us-west-2.compute.amazonaws.com';
+var dbIP       = '54.200.0.50';
 var dbPort     = '27017';
 
 module.exports = {
